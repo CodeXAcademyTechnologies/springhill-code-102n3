055ffac71d226d68c3443fd64fe95cc974b5ecc9
Thomas Basham <Bashamtg@gmail.com>
2023-07-22 12:43:41 -0700

    added class-08 demo

--- class-08/demo/app.js
+++ class-08/demo/app.js
@@ -1,56 +1,51 @@
 "use strict";
 
-function getUsername(){
+function getUsername() {
   let name = prompt("Hi, what's your name");
-  document.write("Hello, " + name)
-  return name
+  document.write("Hello, " + name);
+  return name;
 }
 
-function getFavFoodPlace(){
-
-  let favFood = prompt("What is your favorite fast food restaurant?")
-  return favFood
-
+function getFavFoodPlace() {
+  let favFood = prompt("What is your favorite fast food restaurant?");
+  return favFood;
 }
 
-function setRating(){
-  let starAmount = prompt("How many stars would you like to rate that store?")
-  let parsedNumber = parseInt(starAmount)  
-  let starRating = ''
-  
+function setRating() {
+  let starAmount = prompt("How many stars would you like to rate that store?");
+  let parsedNumber = parseInt(starAmount);
+  let starRating = "";
 
-  // for (initialization; condition; afterthought){ 
+  // for (initialization; condition; afterthought){
   //   code to be executed
   // }
-  for (let i = 0; i < parsedNumber; i = i + 1){
-    starRating += "⭐️"
+  for (let i = 0; i < parsedNumber; i = i + 1) {
+    starRating += "⭐️";
   }
 
-  console.log(starRating)
-  document.write(starRating)
+  console.log(starRating);
+  document.write(starRating);
 }
 
-
-let userGuess = prompt("Guess a Number between 1 and 10")
-let attempts = 3
-let correctAnswer = 8
+let userGuess = prompt("Guess a Number between 1 and 10");
+let attempts = 3;
+let correctAnswer = 8;
 
 // while (condition is true){execute this code}
-while (attempts > 0){
-  if(userGuess == correctAnswer){
-    alert("You got it! The correct answer is " + correctAnswer)
+while (attempts > 0) {
+  if (userGuess == correctAnswer) {
+    alert("You got it! The correct answer is " + correctAnswer);
     break;
   } else {
-    userGuess = prompt("Guess Again!")
-    attempts--
+    userGuess = prompt("Guess Again!");
+    attempts--;
   }
 }
 
-
 // very basic while loop example
-let x = 0
+let x = 0;
 
-while(x < 1000000){
-  console.log(x)
-  x++
+while (x < 1000000) {
+  console.log(x);
+  x++;
 }
